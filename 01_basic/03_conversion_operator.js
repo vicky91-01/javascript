bbce4db688073d02411b4f578d23803ad6ccb94f
vicky kumar001 <vickykumar23499@gmail.com>
2025-02-02 09:53:36 +0000

    comparision

---
+++ 01_basic/03_conversion_operator.js
@@ -0,0 +1,66 @@
+let score = "vicky"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInnumber = Number(score);
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+
+//"33" => 33
+//"33abc" => NaN {Not a Number};
+//true =>1; false =>0; 
+
+let isloggedIn = "himanshu";
+
+let BooleanIsLoggedIn = Boolean(isloggedIn);
+console.log(BooleanIsLoggedIn);
+
+//1 => true; 0 => false;
+//"" => false
+//"himanshu" => true
+
+let someNumber = 34
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+//*****************************Operators************************** */
+
+let value = 3
+
+let negativeValue = -value;
+//console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+
+let str2 = " vicky"
+
+let str3 = (str1 + str2)
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
