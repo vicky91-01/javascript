d4498403297177f85b6133cc593ff46a228889d5
vicky kumar001 <vickykumar23499@gmail.com>
2025-02-02 09:25:29 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,25 @@
+const accountId = 35478;
+let accountEmail = "viky@google.com";
+var accountPass = "12344";
+accountCity = "patna";
+let accountState ;
+
+// accountId = 23; // not allowed because we can define const only one time;
+
+accountEmail = "vicky@microsoft.com"
+accountPass = "123212"
+accountCity = "gaya"
+
+/*
+Prefer not to use var
+because of issue in block and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPass,accountState,accountCity])
+
+
+
+
+
+
+
