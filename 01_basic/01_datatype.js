6d475dfa8892fd40214a3720d3a07e39c5174a3f
vicky kumar001 <vickykumar23499@gmail.com>
2025-02-02 09:27:41 +0000

    datatype

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,29 @@
+"use strict" //treat all JS code as a newer version
+
+// alert (3 + 3); We are using Node.js, not browser
+
+
+
+
+let name = "vicky"
+let age = 20
+let isloggedIn = true
+
+/* PRIMITIVE  DATA TYPES */
+
+//string => ""
+//number => 2 to power 53
+//bigint 
+//boolean => true/false
+//null => Standalone Value
+//undefined =>
+//symbol => unique
+
+/* NON-PRIMITIVE */
+// Object
+// Array
+//Function
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //Object
